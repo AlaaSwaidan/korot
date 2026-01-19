605d56416c311e8f89be58fdb04483ad83917395
AlaaEmad <alaa.emad@bb4it.org>
2026-01-19 03:06:33 +0200

    likecard check price

--- app/Services/Like4AppService.php
+++ app/Services/Like4AppService.php
@@ -49,7 +49,7 @@ class Like4AppService
 
         $response = Http::asMultipart()
             ->timeout(30)
-            ->post('https://taxes.like4app.com/online/topup/products', [
+            ->post('https://taxes.like4app.com/online/topup/checkProductPrice', [
                 'deviceId'     => 'b42bdde70294758891a6590127ec802d37e9e1ee0473c45ee86bd14a1db35cda',
                 'email'        => '3lialmuslem@gmail.com',
                 'password'     => '18c5fd46f0d3ba3929b88feb9e1c4ebe8cd66a5db9a5dd3363f991c727530e5917b96f86a92c4742e9ce77c8a6cf59ec',
