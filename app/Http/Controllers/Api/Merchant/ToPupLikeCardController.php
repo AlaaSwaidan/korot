38e102256f581395385eff1525a8f06cbd5908a7
AlaaEmad <alaa.emad@bb4it.org>
2026-01-19 11:38:52 +0200

    likecard send

--- app/Http/Controllers/Api/Merchant/ToPupLikeCardController.php
+++ app/Http/Controllers/Api/Merchant/ToPupLikeCardController.php
@@ -75,35 +75,35 @@ class ToPupLikeCardController extends Controller
         try {
             $products = $like4App->send($validated);
 
-            if ($products['response'] == 1){
-                $country = CountrySetting::where('code',$validated['country_code'])->first();
-                $merchant_percent = $country ? $country->merchant_percentage : 1;
-                $profit = (float)$request->sellPrice - (float)$request->price;
-                $merchant_profit = $profit * $merchant_percent / 100;
-                $admin_profit = $profit - $merchant_profit;
-
-
-                TopupOrder::create([
-                    'merchant_id'=>$this->user,
-                    'phone'=>$request->phone,
-                    'country_code'=>$request->country_code,
-                    'sku_code'=>$request->skuCode,
-                    'price'=>$request->price,//cost
-                    'sell_price'=>$request->sellPrice,
-                    'receive_amount'=>$request->amount,//receiveAmount for user
-                    'merchant_percentage'=>$merchant_percent,
-                    'merchant_profit'=>$merchant_profit,
-                    'admin_profit'=>$admin_profit
-                ]);
-
-                $this->user->increment('topup_wallet', $merchant_profit);
-
-                $statictics = Statistic::find(1);
-                $statictics->increment('topup_wallet',$admin_profit);
-
-                $statictics->increment('topup_cost',$request->price);
-
-            }
+//            if ($products['response'] == 1){
+//                $country = CountrySetting::where('code',$validated['country_code'])->first();
+//                $merchant_percent = $country ? $country->merchant_percentage : 1;
+//                $profit = (float)$request->sellPrice - (float)$request->price;
+//                $merchant_profit = $profit * $merchant_percent / 100;
+//                $admin_profit = $profit - $merchant_profit;
+//
+//
+//                TopupOrder::create([
+//                    'merchant_id'=>$this->user,
+//                    'phone'=>$request->phone,
+//                    'country_code'=>$request->country_code,
+//                    'sku_code'=>$request->skuCode,
+//                    'price'=>$request->price,//cost
+//                    'sell_price'=>$request->sellPrice,
+//                    'receive_amount'=>$request->amount,//receiveAmount for user
+//                    'merchant_percentage'=>$merchant_percent,
+//                    'merchant_profit'=>$merchant_profit,
+//                    'admin_profit'=>$admin_profit
+//                ]);
+//
+//                $this->user->increment('topup_wallet', $merchant_profit);
+//
+//                $statictics = Statistic::find(1);
+//                $statictics->increment('topup_wallet',$admin_profit);
+//
+//                $statictics->increment('topup_cost',$request->price);
+//
+//            }
             return response()->json([
                 'success' => true,
                 'data' => $products,
