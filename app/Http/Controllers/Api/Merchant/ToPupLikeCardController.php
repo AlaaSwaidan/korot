94eb094313e1173c5883a7b758c28313d31fe6a4
AlaaEmad <alaa.emad@bb4it.org>
2026-01-19 04:12:04 +0200

    likecard send

--- app/Http/Controllers/Api/Merchant/ToPupLikeCardController.php
+++ app/Http/Controllers/Api/Merchant/ToPupLikeCardController.php
@@ -74,8 +74,8 @@ class ToPupLikeCardController extends Controller
 
         try {
             $products = $like4App->send($validated);
-dd($products);
-            if ($products['data']['response'] == 1){
+dd($products['response']);
+            if ($products['response'] == 1){
                 $country = CountrySetting::where('code',$validated['country_code'])->first();
                 $merchant_percent = $country ? $country->merchant_percentage : 1;
                 $profit = (float)$request->sellPrice - (float)$request->price;
